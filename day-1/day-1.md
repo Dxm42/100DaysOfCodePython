3ab2efdfcc09004a5256adc6f0c967fda89cf22c
lucas <lsantana1234@hotmail.com>
2023-09-02 01:56:11 -0300

    initial commit

---
+++ day-1/day-1.md
@@ -0,0 +1,4 @@
+## Gerador de Nomes de Bandas
+Este projeto foi desenvolvido durante o Bootcamp "100DaysOfCode" em Python.
+
+Dia 1: Criamos uma aplicação na qual o usuário informa a cidade onde cresceu e o nome do seu animal de estimação favorito. A partir dessas informações, realizamos a concatenação das strings para gerar o nome da banda.
---
+++ day-1/day-1.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+
+print("Bem vindo ao gerador de nome de banda \n")
+
+#2. Ask the user for the city that they grew up in.
+street = input("Em qual cidade você cresceu:\n")
+#3. Ask the user for the name of a pet.
+pet = input("Qual o seu animal de estimação favorito ?\n")
+#4. Combine the name of their city and pet and show them their band name.
+
+print("O nome da sua banda é " + street + pet)
+#5. Make sure the input cursor shows on a new line:
