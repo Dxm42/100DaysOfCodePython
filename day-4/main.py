bb01fb9f6b718b447dd72abd3a854cfc6af7a8e1
lucas <lsantana1234@hotmail.com>
2023-09-09 01:07:49 -0300

    Day 4 100daysOfCode

---
+++ day-4/main.py
@@ -0,0 +1,11 @@
+# import random
+
+# randomIntegerter = random.randint(1, 10)
+# print(randomIntegerter)
+
+# randomFloat = random.random() * 5
+# print(randomFloat)
+
+# love_score = random.randint(1, 100)
+# print(f"Your lovbe score is {love_score}")
+
