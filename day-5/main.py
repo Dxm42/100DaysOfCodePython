12297daaf2fc8bf9124cda3a63f06136faf54dab
lucas <lsantana1234@hotmail.com>
2023-09-10 20:09:50 -0300

    Day 5 100daysOfCode

---
+++ day-5/main.py
@@ -0,0 +1,21 @@
+# fruits = ["Apple", "Peach", "pear"]
+
+# for fruit in fruits:
+#   print(fruit)
+#   print(fruit + "  Pie")
+# print(fruits)
+
+# student_heights = [180, 124, 165, 173, 189, 169, 146]
+# new_studeent = 0
+# for student in student_heights:
+#   new_studeent +=  student  
+#   average = new_studeent / len(student_heights)
+#   print(round(average))
+# For Loop with Range
+# for number in range(1, 11, 3):
+#   print(number)
+
+# total = 0
+# for number in range(1, 101):
+#   total += number
+# print(total)
