0309de03ec994b57f6630a9938a27eada037ef2f
lucas <lsantana1234@hotmail.com>
2023-09-15 16:41:44 -0300

    caesar cipher

--- day-7/main.py
+++ day-7/main.py
@@ -11,8 +11,22 @@
   
 # greet_with_name("lucas")
 
-def greet_with(name, location):
-  print(f"Hello {name}")
-  print(f"What is it like in {location}")
+# def greet_with(name, location):
+#   print(f"Hello {name}")
+#   print(f"What is it like in {location}")
   
-greet_with(location="Nowhere", name="jane")
+# greet_with(location="Nowhere", name="jane")
+
+def prime_checker(number):
+    is_prime = True
+    for i in range(2 , number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a priem number.")
+
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
